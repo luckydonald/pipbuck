4f479a8a8788a44fbf3538ac8f132a1d13e9d57f
luckydonald <luckydonald@flutterb.at>
2018-12-04 06:16:53 +0100

    [color] Added more methods between more color spaces...

--- src/lib/colorspace/index.js
+++ src/lib/colorspace/index.js
@@ -9,7 +9,7 @@
  *
  * @returns {{h: number, h: number, s: number}}
  */// eslint-disable-next-line no-unused-vars
-function RGBtoHSV(r, g, b) {
+export function RGBtoHSV(r, g, b) {
   if (typeof r === 'object' && g === undefined && b === undefined) {
     return RGBtoHSV(r.r, r.g, r.b);
   }
@@ -23,7 +23,7 @@ function RGBtoHSV(r, g, b) {
     case r: h = (g - b) + d * (g < b ? 6 : 0); h /= 6 * d; break;
     case g: h = (b - r) + d * 2; h /= 6 * d; break;
     case b: h = (r - g) + d * 4; h /= 6 * d; break;
-    default: throw new Error('unknown case.');
+    default: throw new Error('unknown integer case.');
   }
 
   return { h, s, v };
@@ -38,7 +38,7 @@ function RGBtoHSV(r, g, b) {
  *
  * @returns {{r: number, g: number, b: number}}
  */// eslint-disable-next-line no-unused-vars
-function HSVtoRGB(h, s, v) {
+export function HSVtoRGB(h, s, v) {
   if (typeof h === 'object' && s === undefined && v === undefined) {
     return HSVtoRGB(h.h, h.s, h.v);
   }
@@ -67,15 +67,6 @@ function HSVtoRGB(h, s, v) {
 }
 
 /**
- * Rounds `rgb = {r, g, b}` to full numbers.
- * @param rgb
- * @returns {{r: number, g: number, b: number}}
- */// eslint-disable-next-line no-unused-vars
-function roundRGB(rgb) {
-  return { r: Math.round(rgb.r), g: Math.round(rgb.g), b: Math.round(rgb.b) };
-}
-
-/**
  * Converts HSB (0.0 - 1.0) to HSL (0.0 - 1.0).
  *
  * @param h {number|{h: number, s: number, v: number}} Hue.
@@ -85,7 +76,7 @@ function roundRGB(rgb) {
  * @returns {{h: number, s: number, l: number}}
  */
 // eslint-disable-next-line no-unused-vars
-function HSVtoHSL(h, s, v) {
+export function HSVtoHSL(h, s, v) {
   if (typeof h === 'object' && s === undefined && v === undefined) {
     return HSVtoHSL(h.h, h.s, h.v);
   }
@@ -113,9 +104,9 @@ function HSVtoHSL(h, s, v) {
  *
  * @returns {{h: Object, s: number, v: number}}
  */// eslint-disable-next-line no-unused-vars
-function HSLtoHSV(h, s, l) {
-  if (typeof h === 'object' && s === undefined && v === undefined) {
-    return HSLtoHSV(h.h, h.s, h.v);
+export function HSLtoHSV(h, s, l) {
+  if (typeof h === 'object' && s === undefined && l === undefined) {
+    return HSLtoHSV(h.h, h.s, h.l);
   }
 
   const tempL = l * 2;
@@ -128,4 +119,86 @@ function HSLtoHSV(h, s, l) {
     s: convertedS,
     v: convertedV,
   };
+}
+
+/**
+ * Rounds `rgb = {r, g, b}` to full numbers.
+ * @param rgb {{r: number, g: number, b: number}}
+ * @returns {{r: number, g: number, b: number}}
+ */// eslint-disable-next-line no-unused-vars
+export function roundRGB(rgb) {
+  return { r: Math.round(rgb.r), g: Math.round(rgb.g), b: Math.round(rgb.b) };
+}
+
+/**
+ * Converts a color component to hex.
+ *
+ * @param c
+ * @returns {string}
+ *
+ * @see https://stackoverflow.com/a/5624139/3423324
+ */
+function componentToHex(c) {
+  const hex = c.toString(16);
+  return hex.length === 1 ? `0${hex}` : hex;
+}
+
+/**
+ * Converts `rgb = {r, g, b}` to `#rrggbb` hex.
+ *
+ * @param rgb {{r: number, g: number, b: number}}
+ * @returns {string} The hex string `#RRGGBB`
+ * @see https://stackoverflow.com/a/5624139/3423324
+ */// eslint-disable-next-line no-unused-vars
+export function RGBtoHex(rgb) {
+  return `#${componentToHex(rgb.r)}${componentToHex(rgb.g)}${componentToHex(rgb.b)}`;
+}
+
+/**
+ * Converts `#rrggbb` to `rgb = {r, g, b}`.
+ *
+ * @param hex {string} The hex string: `#RRGGBB`.
+ * @returns {{r: number, g: number, b: number}} The RGBs.
+ * @see https://stackoverflow.com/a/5624139/3423324
+ */// eslint-disable-next-line no-unused-vars
+export function HexToRGB(hex) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  const convertedHex = hex.replace(shorthandRegex, (m, r, g, b) => r + r + g + g + b + b);
+
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(convertedHex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16),
+  } : null;
+}
+
+/**
+ * Converts RGB (0.0 - 255.0) to HSL (0.0 - 1.0).
+ *
+ * @param h {number|{h: number, s: number, l: number, a: number}|{h:number,s:number,l:number}} Hue.
+ * @param s {number} Saturation.
+ * @param l {number} Luminescence.
+ * @param a {number} Alpha.
+ * @returns {string}
+ */
+export function hsl(h, s, l, a) {
+  if (typeof h === 'object' && s === undefined && l === undefined && a === undefined) {
+    return hsl(h.h, h.s, h.l, h.a || undefined);
+  }
+  if (a === undefined || a === null) {
+    // use hex "#RRGGBB"
+    console.log({
+      '0_input': { h, s, l },
+      '1_hsv': HSLtoHSV(h, s, l),
+      '2_rgb': HSVtoRGB(HSLtoHSV(h, s, l)),
+      '3_round': roundRGB(HSVtoRGB(HSLtoHSV(h, s, l))),
+      '3_hex': RGBtoHex(roundRGB(HSVtoRGB(HSLtoHSV(h, s, l)))),
+    });
+    return RGBtoHex(roundRGB(HSVtoRGB(HSLtoHSV(h, s, l))));
+  }
+  // use hex "rgba(R, G, B, A)"
+  const { r, g, b } = roundRGB(HSVtoRGB(HSLtoHSV(h, s, l)));
+  return `rgba(${r}, ${g}, ${b}, ${a})`;
 }
