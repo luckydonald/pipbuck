4156b0fcc3fb4ad259903aaa0ad2a70043b0bae7
luckydonald <luckydonald@flutterb.at>
2018-12-06 20:22:26 +0100

    Added colorized img tag to overwrite colors of an img.

---
+++ src/components/Colorized.vue
@@ -0,0 +1,108 @@
+<!-- basically https://stackoverflow.com/q/9163283/3423324 as vue component -->
+<template>
+  <img :src="computedSrc" ref="img" />
+</template>
+
+<script>
+import { RGBtoHSL, HSLtoRGB } from '../lib/colorspace';
+
+export default {
+  name: 'colorized',
+  props: {
+    hue: {
+      default: 0.08,
+      type: Number,
+    },
+    src: {
+      type: String,
+      default: null,
+    },
+    /**
+     * If set to true it will scale the `src` image to the current dimensions this contained `<img>`
+     * element takes.
+     * This might reduce processing power for smaller
+     */
+    preScale: {
+      default: true,
+      type: Boolean,
+    },
+  },
+  data() {
+    return {
+      computedSrc: this.src,
+    };
+  },
+  computed: {
+    img() {
+      const img = document.createElement('img');
+      img.src = this.src;
+      return img;
+    },
+  },
+  watch: {
+    src() { this.loadImage(); },
+    hue() { this.applyImage(); },
+    preScale() { this.applyImage(); },
+  },
+  methods: {
+    loadImage() {
+      console.log('computing img.', this.src, this.hue);
+      // remove old onload listener.
+      this.img.onload = null;
+      // remove old image
+      this.img.src = null;
+      // add new onload
+      this.img.onload = this.imageLoaded;
+      // update src to force loading
+      this.img.src = this.src;
+    },
+    imageLoaded(event) {
+      console.log('loaded img.', event);
+      this.applyImage();
+    },
+    applyImage() {
+      console.log('applying img.');
+      const width = this.preScale ? this.$refs.img.clientWidth : this.img.naturalWidth;
+      const height = this.preScale ? this.$refs.img.clientHeight : this.img.naturalHeight;
+      console.log(
+        'applying img.', width, height,
+      );
+      if (width === 0 || height === 0) {
+        console.log('image not yet loaded.');
+        return;
+      }
+      const canvas = document.createElement('canvas');
+      canvas.width = width;
+      canvas.height = height;
+      const ctx = canvas.getContext('2d');
+      ctx.drawImage(this.img, 0, 0, width, height);
+
+
+      const imgData = ctx.getImageData(0, 0, width, height);
+      const pixels = imgData.data;
+
+      // Loops through all of the pixels and modifies the components.
+      for (let i = 0, n = pixels.length; i < n; i += 4) {
+        const { s, l } = RGBtoHSL(pixels[i + 0], pixels[i + 1], pixels[i + 2]);
+        const { r, g, b } = HSLtoRGB(this.hue, s, l);
+        /* console.log(
+          'rgb>hsl>rgb',
+          `in: { r: ${pixels[i + 0]}, g: ${pixels[i + 1]}, b: ${pixels[i + 2]} },`,
+          `out: { ${r}, ${g}, ${b} },`,
+        ); */
+        pixels[i + 0] = r; // red
+        pixels[i + 1] = g; // blue
+        pixels[i + 2] = b; // green
+        // pixels[i+3] is the transparency, which is kept unchanged.
+      }
+      ctx.putImageData(imgData, 0, 0);
+      console.log('done.');
+      this.computedSrc = canvas.toDataURL('image/png');
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/stats/Status.vue
+++ src/views/stats/Status.vue
@@ -1,18 +1,34 @@
 <template>
   <div>
-    STATUS TEST.
-    <img src="../../assets/pip.png" alt="best pony" title="pip">
+    <input type="number" min="0" max="360" v-model.number="hue" /> lol
+    <colorized
+      :src="require('../../assets/pip.png')"
+      alt="best pony" title="pip" class="img"
+      :hue="hue"
+    />
+    {{ name }} - Level {{ level }}
   </div>
 </template>
 
 <script>
+import Colorized from '../../components/Colorized.vue';
+
 export default {
   name: 'Status',
+  components: { Colorized },
+  data() {
+    return {
+      hue: 0.08,
+      level: 7,
+      name: 'Gunther',
+    };
+  },
 };
 </script>
 
 <style scoped>
-img {
+.img {
   max-width: 20rem;
+  max-height: 20rem;
 }
 </style>
