b2ce8ae66676adf586dd9a487b4cf423c498c90b
luckydonald <luckydonald@flutterb.at>
2018-12-26 02:40:10 +0100

    WIP: Sound test.

--- src/views/xtras/ExtraItems.vue
+++ src/views/xtras/ExtraItems.vue
@@ -9,20 +9,27 @@
         class="sprite"
         @click="play(name)"
       >
-        <div class="sprite-label">{{ name }}</div>
+        <div class="sprite-label">{{ name }} > {{ sprite }}</div>
         <div
-          v-for="sound in sounds[name]" :key="sound.id"
+          v-for="(sound, id) in sounds[name]" :key="id"
           class="progress"
           :style="{ width: (((sound.seek / sound.duration) * 100) || 0) + '%' }"
-        >{{ sound }}
-
-        </div>
+        >{{ sound }}</div>
       </div>
     </div>
+    Moop.<br>
+    <div
+      v-for="(value, key) in sounds" :key="key"
+    >{{ key }}|{{ value }}</div>
+    Meh.<br>
+    {{ sounds }}
+    Mehgh.<br>
   </div>
 </template>
 
 <script>
+import { ui } from '../../sound';
+
 export default {
   name: 'ExtraData',
   props: {
@@ -30,17 +37,26 @@ export default {
     audio: {
       default() {  // default: ui
         // eslint-disable-next-line global-require
-        return require('../../sound').ui;
+        // return require('../../sound').ui;
+        return ui;
       },
       type: Object,
     },
   },
   data() {
     return {
-      sounds: {},
+      sounds: this.createSoundsArray(),
     };
   },
   methods: {
+    createSoundsArray() {
+      return Object.keys(this.audio.config.sprite)
+        .reduce((prev, curr) => {
+          // eslint-disable-next-line no-param-reassign,prefer-destructuring
+          prev[curr] = {};
+          return prev;
+        }, {});
+    },
     play(key) {
       console.log(this);
       // Play the sprite sound and capture the ID.
@@ -50,8 +66,8 @@ export default {
 
       if (Object.keys(this.sounds).indexOf(key) < 0) {
         // create list if not there yet.
-        // this.sounds[key] = [];
-        this.$set(this.sounds, key, []);
+        // this.sounds[key] = {};
+        this.$set(this.sounds, key, {});
       }
       const offset = this.audio.config.sprite[key][0];
       const length = this.audio.config.sprite[key][1];
@@ -62,32 +78,43 @@ export default {
         seek: (play.seek() || 0) - (offset / 1000),
       };
       console.info('pre push', this.sounds[key]);
-      this.sounds[key].push(data);
+      this.$set(this.sounds[key], play.id, data);
       console.info('post push', this.sounds[key]);
 
       // When this sound is finished, remove the progress element.
       play.once('end', () => {
+        play.stop();
         console.warn('end of', key);
-        console.warn('splice check', key, this.sounds[key].lenght, this.sounds[key]);
-        this.sounds[key] = this.sounds[key].filter(s => s.play.id !== play.id);
+        console.warn('splice check', key, play.id, this.sounds[key]);
+        this.$delete(this.sounds[key], play.id);
       });
     },
-  },
-  update() {
-    const keys = Array.keys(this.sounds);
-    for (let ki = 0; ki < keys.length; ki++) {
-      const key = keys[ki];
-      for (let i = 0; i < this.sounds[key].length; i++) {
-        const elem = this.sounds[key][i];
-        elem.seek = (elem.play.seek() || 0) - (elem.offset / 1000);
+    updateTick() {
+      const keys = Object.keys(this.sounds);
+      for (let ki = 0; ki < keys.length; ki++) {
+        const key = keys[ki];
+        const ids = Object.keys(this.sounds[key]);
+        for (let ii = 0; ii < ids.length; ii++) {
+          const id = ids[ii];
+          const elem = this.sounds[key][id];
+          elem.seek = (elem.play.seek() || 0) - (elem.offset / 1000);
+        }
       }
-    }
-    requestAnimationFrame(this.step);
+      requestAnimationFrame(this.updateTick.bind(this));
+    },
+  },
+  mounted() {
+    // Begin the progress step tick.
+    console.log('Wooooop');
+    requestAnimationFrame(this.updateTick.bind(this));
   },
 };
 </script>
 
 <style scoped>
+.sprites {
+  position: relative;
+}
 .progress {
   position: absolute;
   top: 0;
