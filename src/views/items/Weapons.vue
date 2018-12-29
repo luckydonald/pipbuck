3c3d9a098a05947ba7d9e1abb98dae12a4006800
luckydonald <luckydonald@flutterb.at>
2018-12-29 01:37:35 +0100

    WIP: Details columns. Got a nice fading border working.

--- src/views/items/Weapons.vue
+++ src/views/items/Weapons.vue
@@ -2,16 +2,28 @@
   <div class="page">
     <ul class="list">
       <li v-for="weapon in this.weapons" :key="weapon.baseId">
-        <a class="weapon">
+        <a class="weapon" @click="selectItem(weapon.baseId)">
           {{weapon.name}}<span v-if="weapon.amount > 1"> ({{weapon.amount}})</span>
         </a>
       </li>
     </ul>
+    <div class="details">
+      <div class="row">
+        <div class="detail damage"><span>DAM {{activeWeapon['Damage per shot']}}</span></div>
+        <div class="detail weight"><span>WG {{activeWeapon['Weapon weight']}}</span></div>
+        <div class="detail value"><span>VAL {{activeWeapon['Weapon value in caps']}}</span></div>
+      </div>
+      <div class="row">
+          <div class="detail condition">CND [#####  ]</div>
+          <div class="detail ammunition">{{ammunitionText}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import weapons from '../../data/weapons';
+import { ui } from '../../sound';
 
 export default {
   name: 'Weapons',
@@ -19,7 +31,33 @@ export default {
   data() {
     return {
       weapons,
+      activeWeaponId: weapons[0].baseId,
     };
+  },
+  computed: {
+    /**
+     * @return {{type: string, name: string, ammunition: string}}
+     */
+    activeWeapon() {
+      return weapons.filter(weapon => weapon.baseId === this.activeWeaponId)[0];
+    },
+    ammunitionText() {
+      if (
+        Object.keys(this.activeWeapon).indexOf('Ammunition used')
+        && this.activeWeapon['Ammunition used']
+      ) {
+        const type = this.activeWeapon['Ammunition used'];
+        const capacity = this.activeWeapon['Magazine capacity (shots per reload)'];
+        return `${type} (1/${capacity})`;
+      }
+      return '--';
+    },
+  },
+  methods: {
+    selectItem(baseId) {
+      this.activeWeaponId = baseId;
+      this.$emit('pipbuck-play', ui.sounds.gui_select);
+    },
   },
 };
 </script>
@@ -28,10 +66,15 @@ export default {
 $width: 44vw;
 
 ul.list {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
   width: $width;
   text-overflow: ellipsis;
   white-space: nowrap;
-  overflow: hidden;
+  overflow-x: hidden;
+  overflow-y: scroll;
 
   li {
     text-align: left;
@@ -46,6 +89,7 @@ ul.list {
       display: block;
       overflow: hidden;
 
+      // checkbox: http://jsfiddle.net/nN8k7/1/
       &:active:after {
         position: absolute;
         left: 0;
@@ -53,6 +97,36 @@ ul.list {
         font-weight: bold;
         content: "\2610";
       }
+    }
+  }
+}
+
+.details {
+  position: absolute;
+  bottom: 0;
+  right: 0;
+
+  .detail {
+     border-right-width: .5vmin;
+    border-right-style: solid;
+    border-right-color: transparent;
+    //border-image: linear-gradient(to bottom, var(--color-front), transparent) 1 100%;
+     -webkit-border-image: -webkit-gradient(
+        linear, 0 0, 0 100%, from(var(--color-front)), to(rgba(0, 0, 0, 0))
+    ) 1 100%;
+    border-image: linear-gradient(
+        to bottom, var(--color-front), rgba(0, 0, 0, 0)
+    ) 1 100%;
+    position: relative;
+    margin-top: .5vmin;
+    &:after {
+      position: absolute;
+      content: "";
+      top: -.5vmin;
+      left: 0;
+      right: -.5vmin;
+      height: .5vmin;
+      background-color: var(--color-front);
     }
   }
 }
