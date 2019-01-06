4c096403c0dc343fe80d47f5e3358687bda413b8
luckydonald <luckydonald@flutterb.at>
2019-01-06 18:11:54 +0100

    Fixed aid's details to fit the data.

--- src/views/items/Aid.vue
+++ src/views/items/Aid.vue
@@ -2,22 +2,22 @@
   <inventory :items="items" :limit="limit" v-model="activeId">
    <div slot="rows" class="rows">
      <div class="row">
-      <div class="detail damage">
-        <div class="label">DAM</div>
-        <div class="value">{{ activeItem['Damage per shot'] }}</div>
+      <div class="detail damage blank">
       </div>
       <div class="detail weight">
         <div class="label">WG</div>
-        <div class="value">{{ activeItem['Weapon weight'] }}</div>
+        <div class="value">{{ activeItem.weight }}</div>
       </div>
       <div class="detail value">
         <div class="label">VAL</div>
-        <div class="value">{{ activeItem['Weapon value in caps'] }}</div>
+        <div class="value">{{ activeItem.value }}</div>
       </div>
       </div>
       <div class="row">
-        <div class="detail condition">CND [####]</div>
-        <div class="detail ammunition">{{ ammunitionText }}</div>
+        <div class="detail effects">
+          <div class="label">EFFECTS</div>
+          <div class="value">{{ activeItem.Effect }}</div>
+        </div>
       </div>
     </div>
   </inventory>
@@ -53,21 +53,6 @@ export default {
           equipped: (Math.floor(Math.random() * 10)) === 0,
         }));
     },
-    /**
-     * @return {{type: string, name: string, ammunition: string}}
-     */
-    ammunitionText() {
-      if (
-        this.activeItem
-        && Object.keys(this.activeItem).indexOf('Ammunition used')
-        && this.activeItem['Ammunition used']
-      ) {
-        const type = this.activeItem['Ammunition used'];
-        const capacity = this.activeItem['Magazine capacity (shots per reload)'];
-        return `${type} (1/${capacity})`;
-      }
-      return '--';
-    },
   },
   methods: {
     toggleEquip(id, flag) {
@@ -83,103 +68,3 @@ export default {
   },
 };
 </script>
-
-<style scoped lang="scss">
-.page {
-  display: flex;
-  flex-direction: row;
-  height: 100%;
-}
-.scroll-wrapper {
-  order: 0;
-  flex-grow: 1;
-  flex-shrink: 1;
-}
-.apparel {
-  text-align: left;
-  width: 100%;
-}
-
-.details {
-  order: 2;
-  flex-grow: 0;
-  flex-shrink: 0;
-  width: 70vmin;
-  padding-top: -10%;
-  padding-bottom: 10%;
-  // right: 2vmin;
-
-  display: flex;
-  flex-direction: column;
-  align-items: stretch;
-  align-content: flex-end;
-  justify-content: flex-end;
-
-  .image {
-    order: 0;
-    padding: 10vmin;
-    top:10vmin;
-    flex-shrink: 2;
-    box-sizing: border-box;
-    > svg {
-      box-sizing: border-box;
-    }
-  }
-
-  .row{
-    order: 1;
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-  }
-
-  .detail {
-    padding: 0.5vmin 1vmin;
-    white-space: nowrap;
-    width: (100% / 3);
-    margin-left: 1.2vmin;
-
-    display: block;
-    flex-grow: 1;
-    justify-content: space-between;
-
-    // right border is fading
-    border-right-width: .75vmin;
-    border-right-style: solid;
-    border-right-color: transparent;
-     -webkit-border-image: -webkit-gradient(
-        linear, 0 0, 0 100%, from(var(--color-front)), to(rgba(0, 0, 0, 0))
-    ) 1 100%;
-    border-image: linear-gradient(
-        to bottom, var(--color-front), rgba(0, 0, 0, 0)
-    ) 1 100%;
-    // prepare for creating a top border per :after
-    position: relative;
-    margin-top: .75vmin;  // space for border top
-
-    &:after {
-      position: absolute;
-      content: "";
-      top: -.5vmin;
-      left: 0;
-      right: -.5vmin;
-      height: .5vmin;
-      background-color: var(--color-front);
-    }
-
-    &.ammunition {
-      flex-grow: 2;
-    }
-    &.blank {
-      border-right-color: transparent;
-      border-image: none;
-      &:after {
-        background-color: transparent;
-      }
-    }
-  }
-  .label, .value {
-    display: inline;
-  }
-}
-</style>
