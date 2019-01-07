84d4aa71c3053d978ca193c9cd7f6026ad14ebbf
luckydonald <luckydonald@flutterb.at>
2019-01-07 17:46:07 +0100

    Use health bars from game data.

---
+++ src/lib/better-vuex-getter/index.js
@@ -0,0 +1,56 @@
+/** From vuex.esm.js */
+function normalizeNamespace(fn) {
+  // eslint-disable-next-line func-names
+  return function (namespace, map) {
+    if (typeof namespace !== 'string') {
+      // eslint-disable-next-line no-param-reassign
+      map = namespace;
+      // eslint-disable-next-line no-param-reassign
+      namespace = '';
+    } else if (namespace.charAt(namespace.length - 1) !== '/') {
+      // eslint-disable-next-line no-param-reassign
+      namespace += '/';
+    }
+    return fn(namespace, map);
+  };
+}
+
+/** From vuex.esm.js */
+function normalizeMap(map) {
+  return Array.isArray(map)
+    ? map.map(key => ({ key, val: key }))
+    : Object.keys(map).map(key => ({ key, val: map[key] }));
+}
+
+/** From vuex.esm.js */
+const mapGetters = normalizeNamespace((namespace, getters) => {
+  const res = {};
+  normalizeMap(getters).forEach((ref) => {
+    const { key } = ref;
+    let { val } = ref;
+
+    val = namespace + val;
+    res[key] = function mappedGetter() {
+      /* Disabled: We don't wanna limit the namespace on just modules
+      if (namespace && !getModuleByNamespace(this.$store, 'mapGetters', namespace)) {
+        return
+      }
+      */
+      if (process.env.NODE_ENV !== 'production' && !(val in this.$store.getters)) {
+        console.error((`[better-vuex-getter] unknown getter: ${val}`));
+        return undefined;
+      }
+      return this.$store.getters[val];
+    };
+    // mark vuex getter for devtools
+    res[key].vuex = true;
+  });
+  return res;
+});
+
+export {
+  mapGetters,
+};
+export default {
+  mapGetters,
+};
--- src/views/stats/Status.vue
+++ src/views/stats/Status.vue
@@ -3,20 +3,14 @@
     <stats-pony
       class="img" :face-name="'auto'"
       :mane="front" :body="front" :eyes="front" :bars="front" :white="front"
-      :hp="{
-        head: 0.25 + 0.75 * Math.random(),
-        body: 0.25 + 0.75 * Math.random(),
-        legFrontLeft: 0.25 + 0.75 * Math.random(),
-        legFrontRight: 0.25 + 0.75 * Math.random(),
-        legHindLeft: 0.25 + 0.75 * Math.random(),
-        legHindRight: 0.25 + 0.75 * Math.random(),
-      }"
+      :hp="{ head, body, legFrontLeft, legFrontRight, legHindLeft, legHindRight }"
     />
     <div class="player">{{ name }} - Level {{ level }}</div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from '../../lib/better-vuex-getter';
 import StatsPony from '../../components/StatsPony.vue';
 
 export default {
@@ -32,6 +26,13 @@ export default {
     front() {
       return this.$store.state.colorFront;
     },
+    ...mapGetters('game/PlayerInfo/health/parts', [
+      'head', 'body',
+      'legFrontLeft',
+      'legFrontRight',
+      'legHindLeft',
+      'legHindRight',
+    ]),
   },
 };
 </script>
