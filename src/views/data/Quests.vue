ce8b29446e0468d53624d066b458f2956dea86a3
luckydonald <mac2019.git@luckydonald.de>
2021-08-18 18:14:31 +0200

    Route for the quests, albeit it's somehow not working.

---
+++ src/views/data/Quests.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="page">
+    Sample text
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Quests',
+  // eslint-disable-next-line vue/no-unused-components
+  components: { },
+  data() {
+    return {
+    };
+  },
+};
+</script>
+<style scoped lang="scss">
+.page {
+  padding-left: 5vmin;
+  padding-right: 3vmin;
+  padding-bottom: 2vmin;
+}
+</style>
