02ab2648f22d74d348b40768aea348afc024f994
luckydonald <mac2019.git@luckydonald.de>
2021-08-21 18:18:45 +0200

    ESLint --fix doing stuff.

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,8 +11,8 @@ module.exports = {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-plusplus': [2, { allowForLoopAfterthoughts: true }], // https://stackoverflow.com/a/47996411
-    'no-multi-spaces': ["error", { ignoreEOLComments: true }],
-    'no-unused-vars':  ["error", { varsIgnorePattern: "^(ignored)$", argsIgnorePattern: "^(ignored|event)$" }],
+    'no-multi-spaces': ['error', { ignoreEOLComments: true }],
+    'no-unused-vars': ['error', { varsIgnorePattern: '^(ignored)$', argsIgnorePattern: '^(ignored|event)$' }],
     'vue/no-use-v-if-with-v-for': 'off',
   },
   parserOptions: {
