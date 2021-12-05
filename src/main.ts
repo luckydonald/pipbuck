8db1f76f1b2ac69abf55b2ed912cdee219237bdb
luckydonald <mac2019.git@luckydonald.de>
2021-12-05 18:00:05 +0100

    Duplicated the old index.js to be index.ts. (Basically copying everything over)

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,40 @@
-import { createApp } from 'vue';
+/**
+ * Main app; data store. Loads App.vue.
+ */
+import Vue, { createApp } from 'vue';
+import Meta from 'vue-meta';
+import VueAnalytics from 'vue-analytics';
+import screenfull from 'screenfull';
+import './lib/audiocontect-patch';
 import App from './App.vue';
+import router from './router';
+import store from './state';
 
-createApp(App).mount('#app');
+Vue.use(Meta, {
+  keyName: 'head', // the component option name that vue-meta looks for meta info on.
+  attribute: 'data-vue-meta', // the attribute name vue-meta adds to the tags it observes
+  ssrAttribute: 'data-vue-meta-server-rendered', // the attribute name that lets vue-meta know that meta info has already been server-rendered
+  tagIDKeyName: 'm-key', // the property name that vue-meta uses to determine whether to overwrite or append a tag
+});
+
+Vue.use(VueAnalytics, {
+  id: process.env.NODE_ENV !== 'production' ? 'UA-47744366-5' : 'UA-47744366-4',
+  router,
+  autoTracking: {
+    pageviewOnLoad: true,
+    // page: false,
+    screenview: true,
+  },
+  batch: {
+    enabled: true, // enable/disable
+    amount: 2, // amount of events fired
+    delay: 500, // delay in milliseconds
+  },
+});
+
+const ignored = createApp(App).use(router).use(store).mount('#app');
+
+if (process.env.NODE_ENV !== 'production') {
+  global.$vm = ignored;
+}
+screenfull.request();
