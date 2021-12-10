5ec6688d8649807157eeb80ad386a620d76e1210
luckydonald <mac2019.git@luckydonald.de>
2021-12-10 14:24:25 +0100

    Using vite-svg-loader, as vue-svg-loader was broken.

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import svgLoader from 'vite-svg-loader';
 import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(), svgLoader()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, '/src'),
